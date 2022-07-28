748252123b38b8df08dcdd5615db9fbff7028ed0
Babalola Timothy <babalolatimothy2@gmail.com>
2022-07-28 12:53:33 +0000

    changed v2

--- src/assignment.js
+++ src/assignment.js
@@ -47,7 +47,7 @@ assignment.countEvenNumbers = countEvenNumbers;
  * Challenge - 3
  * 
  * Given an array of numbers representing temperatures in Celsius,
- * convert each number to Fahrenheit and return a new array containing the converted
+ * convert each number to Fahrenheits and return a new array containing the converted
  * temperatures in Fahrenheit. Decimal figures in the converted values in Fahrenheit should be removed.
  * E.g 51.21 should just be 51 (hint: Math.trunc(...) function)
  * If you did Challenge - 3, remove the comment in the line just after this function
